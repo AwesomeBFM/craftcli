1480750c4d53f6af07cdab792cb4409946a026cb
AwesomeBFM <55819926+AwesomeBFM@users.noreply.github.com>
2024-02-15 09:43:45 -0700

    project setup

---
+++ .history/main_20240215085759.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 AwesomeBFM <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/AwesomeBFM/CraftCLI/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ .history/main_20240215085813.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 AwesomeBFM <55819926+AwesomeBFM@users.noreply.github.com>
+
+*/
+package main
+
+import "github.com/AwesomeBFM/CraftCLI/cmd"
+
+func main() {
+	cmd.Execute()
+}
